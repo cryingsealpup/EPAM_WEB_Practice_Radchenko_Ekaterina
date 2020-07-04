61550830254d8ca0fa1829ca89f3268bc435bfb0
EkaterinaRadchenko <ekatherinered@gmail.com>
2020-07-04 11:01:21 +0400

    update navbar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
-import "./styles/menu.scss";
-
-var navItems = document.querySelectorAll(".nav__link");
-navItems.forEach(function(e, i) {
-	e.addEventListener("click", function(e) {
-		navItems.forEach(function(e2, i2) {
-			e2.classList.remove("nav__link--active");
-		})
-		this.classList.add("nav__link--active");
-	});
-});
-alert("js is here");
+require("./scripts/menuHighlight.js")
